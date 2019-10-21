4eb16a52d99a6574a3c627ac5e342d45ad7e810a
Alex Kyriakidis <hootlex@icloud.com>
2019-10-21 21:46:09 +0200

    extract gh logic to useFetchGithubUser

--- src/components/GithubUserCardCompo.vue
+++ src/components/GithubUserCardCompo.vue
@@ -1,23 +1,26 @@
 <template>
     <div class="ui card">
-      <div class="image">
-        <img :src="user.avatar_url">
-      </div>
-      <div class="content">
-        <a :href="`https://github.com/${username}`" class="header">{{user.name}}</a>
-        <div class="meta">
-          <span class="date">Joined in {{user.created_at}}</span>
+      <template v-if="loadingState === 'success'">
+        <div class="image">
+          <img :src="user.avatar_url">
         </div>
-        <div class="description">
-          {{user.bio}}
+        <div class="content">
+          <a :href="`https://github.com/${username}`" class="header">{{user.name}}</a>
+          <div class="meta">
+            <span class="date">Joined in {{user.created_at}}</span>
+          </div>
+          <div class="description">
+            {{user.bio}}
+          </div>
         </div>
-      </div>
-      <div class="extra content">
-        <a :href="`https://github.com/${username}?tab=followers`">
-          <i class="user icon"></i>
-          {{user.followers}} Friends
-        </a>
-      </div>
+        <div class="extra content">
+          <a :href="`https://github.com/${username}?tab=followers`">
+            <i class="user icon"></i>
+            {{user.followers}} Friends
+          </a>
+        </div>
+      </template>
+      <span v-if="loadingState === 'loading'">Loading user....</span>
     </div>
 </template>
 
@@ -32,21 +35,30 @@
       }
     },
     setup (props) {
-      console.log(props)
-      let user = ref({})
-      axios({
-        method: 'GET',
-        url: `https://api.github.com/users/${props.username}?token=94b83ad8d43b186ff66c5b81b56333a11896f65d`,
-        headers: {
-          Authorization: `Bearer 94b83ad8d43b186ff66c5b81b56333a11896f65d`,
-          "Content-Type": "application/json"
-        }
-      })
-        .then(response => {
-            user.value = response.data
-        })
-      return {user}
+      const {user, loadingState} = userFetchGithubUser(props)
+      return {user, loadingState}
     }
+  }
+
+  function userFetchGithubUser(props) {
+    let user = ref({})
+    let loadingState = ref('loading')
+    axios({
+      method: 'GET',
+      url: `https://api.github.com/users/${props.username}?token=94b83ad8d43b186ff66c5b81b56333a11896f65d`,
+      headers: {
+        Authorization: `Bearer 94b83ad8d43b186ff66c5b81b56333a11896f65d`,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        setTimeout(() => {
+          console.log('🎉')
+          loadingState.value = 'success'
+          user.value = response.data
+        }, 3000)
+      })
+    return {user, loadingState}
   }
 </script>
 
