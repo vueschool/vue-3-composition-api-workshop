89f68c8b5d264d563302b1612ee8d36a2349ccf4
Alex Kyriakidis <hootlex@icloud.com>
2020-01-16 17:51:08 +0100

    extract fetching to function

--- src/components/CharacterCards.vue
+++ src/components/CharacterCards.vue
@@ -36,22 +36,28 @@
   import axios from 'axios'
   import orderBy from 'lodash.orderby'
   import {ref} from '@vue/composition-api'
+
+  const useFetchAllCharacters = () => {
+    const loadingState = ref(null)
+    const characters = ref([])
+
+    const fetchAllCharacters = () => {
+      loadingState.value = 'loading'
+      axios.get('https://rickandmortyapi.com/api/character')
+        .then(response => {
+          setTimeout(() => {
+            loadingState.value = 'success'
+            characters.value = response.data.results
+          }, 1000)
+        })
+    }
+
+    return {loadingState, characters, fetchAllCharacters}
+  }
+
   export default {
     setup () {
-      const loadingState = ref(null)
-      const characters = ref([])
-
-      const fetchAllCharacters = () => {
-        loadingState.value = 'loading'
-        axios.get('https://rickandmortyapi.com/api/character')
-          .then(response => {
-            setTimeout(() => {
-              loadingState.value = 'success'
-              characters.value = response.data.results
-            }, 1000)
-          })
-      }
-
+      const {loadingState, characters, fetchAllCharacters} = useFetchAllCharacters()
       return {loadingState, characters, fetchAllCharacters}
     },
     data () {
